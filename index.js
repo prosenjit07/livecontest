ad358ad407222346f418fd1d9aca77ebbdfc15da
Prosenjit <prosenjitbiswas983@gmail.com>
2023-06-25 14:32:58 +0600

    filter add

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ function sendCategory(index) {
     fetchAPI(websiteAPI[index]);
 }
 
+function time1(seconds) {
+    max_duration("all");
+}
+
+function time2(seconds) {
+    min_duration("all");
+}
+
+
 fetchAPI("all");
 
 
@@ -38,6 +47,110 @@ fetch('Assets/json/codingQuota.json')
     })
     .catch(err => console.error(err));
 
+
+function min_duration(webName) {
+    fetch(`https://kontests.net/api/v1/${webName}`)
+        .then(response => response.json())
+        .then(data => {
+            
+            data.sort((a, b) => {
+                var start_date = new Date(b.duration);
+                var end_date = new Date(a.duration);
+                return end_date.getTime() - start_date.getTime();
+            });
+
+            console.log(data);
+           
+            let str = "";
+            for (key in data) {
+                str += `<div class="card mb-3">
+                        <img src="https://source.unsplash.com/1000x400/?${category[randomCategory()]},${category[randomCategory()]}" class="card-img-top"
+                            alt="Image">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between">
+                                <h5 class="card-title">${data[key].name}</h5>
+                                <div>
+                                    <span class="badge text-bg-success">${data[key].site != undefined ? data[key].site : website[websiteAPI.indexOf(webName)]} </span>
+                                    ${data[key].in_24_hours = true ? `<span></span>` : `<span class="badge text-bg-danger">Ends in 24 hour </span>`}
+                                   
+                                </div >
+                            </div >
+                            <p class="card-text">${json[key].text}</p>
+                            <div class="d-grid gap-1">
+                                <a href="${data[key].url}" target="_blank">
+                                <button class="btn btn-block btn-primary">Join Contest</button>
+                                </a>
+                            </div>
+                            </div >
+                    <div class="d-flex justify-content-between card-footer text-muted">
+                        <div class="timeText ">
+                            Start Date:${formatedDate(data[key].start_time)}
+                        </div>
+                        <div class="timeText ">
+                            Duartion: ${toDay(data[key].duration)} Days 
+                        </div>
+                        <div class="timeText ">
+                            End Date: ${formatedDate(data[key].end_time)}
+                        </div>
+                    </div>
+                  </div > `;
+            }
+            cardBody.innerHTML = str;
+        }).catch(err => console.error(err));
+
+}
+
+function max_duration(webName) {
+    fetch(`https://kontests.net/api/v1/${webName}`)
+        .then(response => response.json())
+        .then(data => {
+            
+            data.sort((a, b) => {
+                var start_date = new Date(a.duration);
+                var end_date = new Date(b.duration);
+                return end_date.getTime() - start_date.getTime();
+            });
+
+            console.log(data);
+           
+            let str = "";
+            for (key in data) {
+                str += `<div class="card mb-3">
+                        <img src="https://source.unsplash.com/1000x400/?${category[randomCategory()]},${category[randomCategory()]}" class="card-img-top"
+                            alt="Image">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between">
+                                <h5 class="card-title">${data[key].name}</h5>
+                                <div>
+                                    <span class="badge text-bg-success">${data[key].site != undefined ? data[key].site : website[websiteAPI.indexOf(webName)]} </span>
+                                    ${data[key].in_24_hours = true ? `<span></span>` : `<span class="badge text-bg-danger">Ends in 24 hour </span>`}
+                                   
+                                </div >
+                            </div >
+                            <p class="card-text">${json[key].text}</p>
+                            <div class="d-grid gap-1">
+                                <a href="${data[key].url}" target="_blank">
+                                <button class="btn btn-block btn-primary">Join Contest</button>
+                                </a>
+                            </div>
+                            </div >
+                    <div class="d-flex justify-content-between card-footer text-muted">
+                        <div class="timeText ">
+                            Start Date:${formatedDate(data[key].start_time)}
+                        </div>
+                        <div class="timeText ">
+                            Duartion: ${toDay(data[key].duration)} Days 
+                        </div>
+                        <div class="timeText ">
+                            End Date: ${formatedDate(data[key].end_time)}
+                        </div>
+                    </div>
+                  </div > `;
+            }
+            cardBody.innerHTML = str;
+        }).catch(err => console.error(err));
+
+}
 
 function fetchAPI(webName) {
     fetch(`https://kontests.net/api/v1/${webName}`)
